0ae56e1a3acab2c577d29e77f5075889b012dc5b
SinisterSoda <mikeg@pixelacademy.org>
2017-08-30 10:17:14 -0400

    Fix first fix by not being an idiot (check should be == 'undefined' not != 'undefined'... dumba**)

--- src/App.js
+++ src/App.js
@@ -667,7 +667,7 @@ class App extends Component {
 
         $(".birthday").removeClass("birthday");
 
-        if (typeof firstMember != 'undefined'){
+        if (typeof firstMember == 'undefined'){
             console.log("Member for key " + memberKey + " Undefined. Aborting", this.state.members, this.state.members.length);
             return;
         }
