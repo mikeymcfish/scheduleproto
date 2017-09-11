f6d7b1c6ba57f77f7c8b26d71a1ab32d1a39397f
Adam Meech <meech.adam@gmail.com>
2017-09-11 08:23:24 -0500

    Update amount of items in list

--- src/calendar/list/index.js
+++ src/calendar/list/index.js
@@ -4,8 +4,11 @@ import ListItem from './item';
 
 import moment from 'moment';
 import uniqBy from 'lodash/uniqBy';
+import flattenDeep from 'lodash/flattenDeep';
 
 import './../../styles/list.css';
+
+const MAX_LIST_EVENTS = 50;
 
 export default class extends Component {
     getEvents(allEvents) {
@@ -29,33 +32,40 @@ export default class extends Component {
         }
 
         // This is pretty janky, need to find a better way
+        var listEvents = [];
         var notFound = true;
         while (notFound) {
             var dateString = today.format('M-D');
 
             if (dayStringOrdered[dateString]) {
-                return dayStringOrdered[dateString].map(event => {
+                var events = dayStringOrdered[dateString].map(event => {
                     event.dateObject = today;
                     return event;
                 });
+
+                listEvents.push(events);
             }
 
             // Stops this from being an infinite loop
-            if (today.isAfter(moment().add(60, 'days'))) {
-                return null;
+            if (today.isAfter(moment().add(60, 'days')) || listEvents.length > MAX_LIST_EVENTS) {
+                return flattenDeep(listEvents);
             }
 
-            today.add(1, 'days');
+            today = today.clone().add(1, 'days');
         }
     }
 
     render() {
         const events = this.getEvents(this.props.allEvents);
 
-        if (!events || this.props.hide) {
+        if (!events || events.length === 0 || this.props.hide) {
             return null;
         }
 
-        return <div id="list-container">{events.map(event =><ListItem event={ event } setViewDay={ this.props.setViewDay } />)}</div>;
+        return <div id="list-container">
+            {events.map(event => {
+                return <ListItem event={ event } setViewDay={ this.props.setViewDay } />;
+            })}
+        </div>;
     }
 }
